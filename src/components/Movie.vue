d81f3c9cf2d8a8ad96acdb27bc9b8e00441160ca
Phan Quang <phanlequang99@gmail.com>
2019-06-28 01:33:42 +0700

    Reformat code

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,53 +1,50 @@
 <template>
-    <div>
-        <b-card-group style="margin:30px;" v-for="(item,index) in $store.getters.listFilm||Search" deck>
-            <b-card
-                    bg-variant="dark"
-                    img-alt="Card Image"
-                    text-variant="white"
-                    v-bind:title="item.imdbID"
-                    v-bind:sub-title="item.Year"
-            >
-                <b-card-text>
-                    {{item.Title}}
-                </b-card-text>
-                <b-button v-bind:href="'#/detail/'+item.imdbID" variant="primary">View</b-button>
-            </b-card>
-        </b-card-group>
-    </div>
+  <div>
+    <b-card-group style="margin:30px;" v-for="(item,index) in $store.getters.listFilm" deck>
+      <b-card
+        bg-variant="dark"
+        img-alt="Card Image"
+        text-variant="white"
+        v-bind:title="item.imdbID"
+        v-bind:sub-title="item.Year"
+      >
+        <b-card-text>{{item.Title}}</b-card-text>
+        <b-button v-bind:href="'#/detail/'+item.imdbID" variant="primary">View</b-button>
+      </b-card>
+    </b-card-group>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Movie',
-        props: {
-            name: String
-        },
-        data: function () {
-            return {
-                Search: []
-            }
-        },
-        mounted() {
-            //load something
-        }
-    }
+export default {
+  name: "Movie",
+  props: {
+    name: String
+  },
+  data: function() {
+    return {
+      Search: []
+    };
+  },
+  mounted() {
+    //load something
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    h3 {
-        margin: 40px 0 0;
-    }
+h3 {
+  margin: 40px 0 0;
+}
 
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 </style>
--- src/store.js
+++ src/store.js
@@ -1,26 +1,26 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import axios from 'axios'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-    state: {
-        fileName: '',
-        listFilm: []
-    },
-    mutations: {
-        change(state, filmName) {
-            state.filmName = filmName;
-            axios
-                .get(`http://www.omdbapi.com/?apikey=41e34b17&s=${filmName}`)
-                .then(response => {
-                    state.listFilm = response.data.Search
-                })
-        }
-    },
-    getters: {
-        listFilm: state => state.listFilm,
-        filmName: state => state.filmName
+  state: {
+    fileName: "",
+    listFilm: []
+  },
+  mutations: {
+    change(state, filmName) {
+      state.filmName = filmName;
+      axios
+        .get(`http://www.omdbapi.com/?apikey=41e34b17&s=${filmName}`)
+        .then(response => {
+          state.listFilm = response.data.Search;
+        });
     }
+  },
+  getters: {
+    listFilm: state => state.listFilm,
+    filmName: state => state.filmName
+  }
 });
